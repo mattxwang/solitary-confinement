bbdfea6f73465d9bfe8951b1c861cc50433325e9
Matthew Wang <matt@matthewwang.me>
2019-09-05 23:55:37 -0700

    Updates

---
+++ precache-manifest.9cdc7063eb436c9b77248ac25c6abb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3710efb06d8849f461743ca233ac73c1",
+    "url": "/solitary-confinement/index.html"
+  },
+  {
+    "revision": "80d38ffb40cfa1b692c2",
+    "url": "/solitary-confinement/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "522f9f75c71b4ebde7cc",
+    "url": "/solitary-confinement/static/css/main.ed571ad0.chunk.css"
+  },
+  {
+    "revision": "80d38ffb40cfa1b692c2",
+    "url": "/solitary-confinement/static/js/2.67038e0f.chunk.js"
+  },
+  {
+    "revision": "522f9f75c71b4ebde7cc",
+    "url": "/solitary-confinement/static/js/main.6a3a916f.chunk.js"
+  },
+  {
+    "revision": "434a401c710f86237769",
+    "url": "/solitary-confinement/static/js/runtime~main.0db51537.js"
+  }
+]);
