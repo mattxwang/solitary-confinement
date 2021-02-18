8a60d6d7ed4914fbe76ea690dd071a3e02ec353b
Matt Wang <matt@matthewwang.me>
2021-02-17 19:42:04 -0800

    change gh username

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-const githubLink = "https://github.com/malsf21/solitary-confinement";
+const githubLink = "https://github.com/mattxwang/solitary-confinement";
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
             made by <a href="https://matthewwang.me" target="_blank" rel="noopener noreferrer">matt wang</a> with react and react-bootstrap | cards by <a href="https://www.me.uk/cards/" target="_blank" rel="noopener noreferrer">Adrian Kennard</a>
         </p>
       </Container>
-      
+
     </div>
   );
 }
